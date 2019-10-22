46007eb197b7e9f14e0122efb6c9c3b0544f5259
miia <miia@com.tw>
2019-10-22 16:50:28 +0800

    second commit

--- app/src/main/java/com/sen/guess/MainActivity.java
+++ app/src/main/java/com/sen/guess/MainActivity.java
@@ -3,34 +3,52 @@ package com.sen.guess;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    String TAG = MainActivity.class.getSimpleName();
+    int secret = new Random().nextInt(10) + 1;
+    private TextView number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "secret: " + secret);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        number = findViewById(R.id.guess);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
     }
 
+    public void test(View view) {
+        int num = Integer.parseInt(number.getText().toString());
+        if (num > secret) {
+            Toast.makeText(MainActivity.this, "smaller", Toast.LENGTH_LONG).show();
+        } else if (num < secret) {
+            Toast.makeText(MainActivity.this, "largerer", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(MainActivity.this, "You guess it!", Toast.LENGTH_LONG).show();
+
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
